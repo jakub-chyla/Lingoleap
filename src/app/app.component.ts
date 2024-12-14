565f3b78936e27590183965f1b2cf6fe91efe6ce
jakub-chyla <jakub.bchyla@gmail.com>
2024-12-14 10:59:26 +0100

    add docker file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatButton} from "@angular/material/button";
 import {MatCard, MatCardContent} from "@angular/material/card";
+import {Word} from "./word";
+
+const wordsList: Word[] = [
+  new Word("conducted by", "prowadzone przez"),
+  new Word("distortion", "zniekształcenie"),
+  new Word("neglect", "zaniedbanie"),
+  new Word("emphasis", "podkreślenie"),
+  new Word("persuasive", "przekonywający"),
+  new Word("discrepancy", "rozbieżność"),
+  new Word("abbreviation", "skrót"),
+  new Word("consistent", "spójny, konsekwentny"),
+  new Word("resilient", "odporny"),
+  new Word("verbose", "gadatliwy"),
+  new Word("persistent", "wytrwały"),
+  new Word("imposter", "oszust"),
+  new Word("clerk", "urzędnik"),
+  new Word("rugged", "chropowaty"),
+  new Word("rouge", "łobuz"),
+];
 
 @Component({
   selector: 'app-root',
@@ -13,6 +32,26 @@ import {MatCard, MatCardContent} from "@angular/material/card";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'english-words';
+export class AppComponent implements OnInit {
+  wordsList: Word[] = [];
+  currentWord!: Word;
+
+  ngOnInit() {
+    this.wordsList = wordsList;
+    this.displayRandomWord();
+  }
+
+  displayRandomWord() {
+    const randomIndex = Math.floor(Math.random() * this.wordsList.length);
+    this.currentWord = this.wordsList[randomIndex];
+  }
+
+  readText() {
+    if ('speechSynthesis' in window) {
+      const speech = new SpeechSynthesisUtterance(this.currentWord.english);
+      window.speechSynthesis.speak(speech);
+    } else {
+      alert('Text-to-speech is not supported in your browser.');
+    }
+  }
 }
---
+++ src/app/word.ts
@@ -0,0 +1,9 @@
+export class Word {
+  english: string;
+  polish: string;
+
+  constructor(english: string, polish: string) {
+    this.english = english;
+    this.polish = polish;
+  }
+}
